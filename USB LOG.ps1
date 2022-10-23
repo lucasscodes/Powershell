1d2bde9a3173aa71f1ab6f4f9e79d6a867ee0e2b
lucasscodes <116492976+lucasscodes@users.noreply.github.com>
2022-10-23 15:13:11 +0200

    Add files via upload
    This is just mirrored from reddit! All credits go to https://www.reddit.com/user/justchillokay/

---
+++ USB LOG.ps1
@@ -0,0 +1,34 @@
+#Source: https://www.reddit.com/r/PowerShell/comments/9zebvv/comment/eaic9mc/?utm_source=share&utm_medium=web2x&context=3
+#Seems plausible that you could run a script on (the occurrence of) one of these events to collect more detailed information.
+$EventLog = New-Object System.Diagnostics.Eventing.Reader.EventLogConfiguration 'Microsoft-Windows-DriverFrameworks-UserMode/Operational'
+
+if($EventLog.IsEnabled -eq $True)
+{
+    [System.Collections.ArrayList]$EventList = @()
+        $filter = @{
+            Logname  = 'Microsoft-Windows-DriverFrameworks-UserMode/Operational'
+            Level    = 0, 1, 2, 3, 4, 5
+            ID	     = 2003, 2004, 2005, 2100, 2101, 2102, 2103, 2104, 2105, 2106, 2107, 2108, 2109
+        }
+    $events = Get-WinEvent $filter
+    foreach($event in $events)
+    {
+
+        $XML = [xml]$event.ToXml()
+        [void]$EventList.Add([PSCustomObject]@{ 
+            LifetimeId   = $XML.Event.UserData.UMDFHostDeviceRequest.LifetimeId
+            InstanceId   = $XML.Event.UserData.UMDFHostDeviceRequest.InstanceId
+            EventID      = $XML.Event.System.EventID
+            Computer     = $XML.Event.System.Computer
+            TimeCreated  = [datetime]$XML.Event.System.TimeCreated.SystemTime
+        })
+    }
+
+} else 
+{
+    $EventLog.IsEnabled = $True
+    $EventLog.SaveChanges()
+
+}
+
+$EventList
